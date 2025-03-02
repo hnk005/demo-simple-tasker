c62bd581b368da8ab58d2d921d01ef0763865976
hnk005 <huynhnamkha512020@gmail.com>
2025-03-02 15:32:22 +0700

    Change

--- assets/index-Dt5WVYJU.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-Dp_26h87.js";const t=({fullScreen:r=!1})=>e.jsx("div",{className:`flex justify-center items-center ${r?"fixed inset-0 bg-white/70 backdrop-blur-sm":"py-4"}`,children:e.jsx("div",{className:"w-12 h-12 border-4 border-green-600 border-t-transparent rounded-full animate-spin"})});export{t as default};
